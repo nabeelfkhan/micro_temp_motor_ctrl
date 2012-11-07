371647d9933981a1a79603c84cb2d022cdb39a2b
Jason Randall <jason.randall2@gmail.com>
2012-11-07 07:18:33 -0500

    'Real' initial commit because I'm careless
    The project in the initial commit was not initialized properly and
    probably wouldn't work if you cloned it.  This one will..

---
+++ Temp Motor Controller/CODE/Events.h
@@ -0,0 +1,40 @@
+/** ###################################################################
+**     Filename  : Events.h
+**     Project   : Temp_Motor_Controller
+**     Processor : MC9S12C128CFU16
+**     Component : Events
+**     Version   : Driver 01.04
+**     Compiler  : CodeWarrior HC12 C Compiler
+**     Date/Time : 11/7/2012, 7:14 AM
+**     Abstract  :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings  :
+**     Contents  :
+**         No public methods
+**
+** ###################################################################*/
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+
+#pragma CODE_SEG DEFAULT
+
+
+/* END Events */
+#endif /* __Events_H*/
+
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 3.02 [04.44]
+**     for the Freescale HCS12 series of microcontrollers.
+**
+** ###################################################################
+*/
